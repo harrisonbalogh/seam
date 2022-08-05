133382985ba8d5c825875232f850a71d580990bd
Harrison Balogh <harrisonbalogh@gmail.com>
2022-08-05 12:29:31 -0700

    seam-lib package. Constant AUTH_DOMAIN

--- hx-seam-lib/Constants.js
+++ hx-seam-lib/Constants.js
@@ -1,7 +1,6 @@
 
+export const AUTH_DOMAIN = 'https://harrisonbalogh.com'
 export const DOMAIN = 'wss://hxdev.com'
-// export const DOMAIN = 'ws://localhost'
-
 export const RELAY_SERVER_URL = `${DOMAIN}/api/seam`
 
 export const STUN_SERVER_URL = 'stun:harrisonbalogh.com:3478'
